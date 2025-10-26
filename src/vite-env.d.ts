226409e051f7926aae6a510206a1b00362d98709
Soham <sohambhutkar1@gmail.com>
2025-10-26 10:34:43 +0000

    Refactor: Implement security and performance improvements

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="vite/client" />
+
+declare global {
+  interface Window {
+    $: any;
+    jQuery: any;
+    
+    // Flipbook global functions
+    loadFlipbook: (
+      url: string, 
+      isSinglePage: boolean, 
+      page: number, 
+      pdfId: string
+    ) => void;
+
+    getLastPage: (pdfId: string) => Promise<number>;
+  }
+}
+
+export {};
